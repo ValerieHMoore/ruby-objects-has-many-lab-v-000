3e9168666afa8e21503c57ecfe9e4e8f782cc8be
Valerie Moore <periwinkle17@outlook.com>
2018-07-02 17:37:03 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -22,7 +22,7 @@ class Author
   #       betty.add_post_by_title("My Great Blog Post")
   #       expect(betty.posts.last.title).to eq("My Great Blog Post")
   #       expect(betty.posts.last.author).to eq(betty)
-    posts.title = self
+    posts.title.author = self
   end
   
  end
